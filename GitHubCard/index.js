b97a3686acf9b50c363b342792bde5cfe115ea33
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-11 20:07:37 +0100

    WIP: setup get response

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/chris-ian-jones')
+  .then(data => {
+    console.log('response:', data)
+  })
+  .catch(error => {
+    console.log('error message')
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
